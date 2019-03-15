64471d649b9b48e42e345e5bc4fc0cbed13f1bb6
Rich Castagna <idev.tru@gmail.com>
2019-03-15 13:39:33 -0400

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Richard Castagna on 3/15/19.
+//  Copyright © 2019 Rich Castagna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
